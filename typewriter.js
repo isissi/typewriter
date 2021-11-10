72625a0aa2553e4bae0523f4770eb76f6071b525
isissi <isissi_525@hotmail.com>
2021-11-09 20:49:10 -0700

    update

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,11 @@
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs\n";
+
+let time = 0;
+
+for (const char of sentence) {
+  time += 50;
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, time);
+  
+}
